7608f8a6064524d4bdf791a76f84f2d9b8c3c546
nbergstrand <bergstrand.niklas@gmail.com>
2021-09-05 14:54:21 +0100

    Added interactive map with ScriptableObjects

---
+++ Assets/Scripts/InfoPanel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoPanel : MonoBehaviour
+{
+    [SerializeField]
+    Text _titleText;
+
+    [SerializeField]
+    Text _infoText;
+
+    [SerializeField]
+    Text _animalText;
+
+    [SerializeField]
+    Image _animalImage;
+
+    
+
+    public void FillPanel(PanelTemplate info)
+    {
+        _titleText.text = info._titleText;
+        _infoText.text = info._infoText;
+        _animalText.text = info._animalText;
+        _animalImage.sprite = info._animalSprite;
+
+
+
+    }
+
+}
---
+++ Assets/Scripts/PanelTemplate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Info Panel", menuName = "Info Panel")]
+public class PanelTemplate : ScriptableObject
+{
+
+    
+    public string _titleText;
+
+    public string _infoText;
+
+    public string _animalText;
+
+    public Sprite _animalSprite;
+
+
+}
