b2f40bd4ae868da9b72692c70b75c10ff035fc43
nbergstrand <bergstrand.niklas@gmail.com>
2021-09-05 15:15:46 +0100

    Added analytics

---
+++ Assets/Scripts/AnalyticsEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class AnalyticsEvent : MonoBehaviour
+{
+    public void OnButtonClicked(string eventInfo)
+    {
+        Analytics.CustomEvent(eventInfo);
+    }
+}
